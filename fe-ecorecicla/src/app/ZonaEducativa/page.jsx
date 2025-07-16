07d445b8d31e2e4bfc01da70000014ba7dbd5d5a
Gofra2003 <121211543+Gofra2003@users.noreply.github.com>
2025-07-16 16:30:42 -0500

    Cambio zona Educativa
    Borre un frameborder de los que se ponían en el iframe, porque daba error en next xd

--- fe-ecorecicla/src/app/ZonaEducativa/page.jsx
+++ fe-ecorecicla/src/app/ZonaEducativa/page.jsx
@@ -45,7 +45,7 @@ export default function ZonaEducativa() {
               <div className="aspect-video w-full max-w-md rounded-lg shadow-lg overflow-hidden mx-auto lg:mx-0">
                <iframe width="560" height="315" src="https://www.youtube.com/embed/cCOfCFzQvCc?si=LmwYn4f_W6pfXHuK"
                 title="YouTube video player" 
-                frameborder="0" 
+                
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                  referrerpolicy="strict-origin-when-cross-origin"
                 allowfullscreen></iframe>
