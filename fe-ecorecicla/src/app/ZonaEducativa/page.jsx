d1a991091156eefeabfdc31ef3a4d5f93ab91bc6
Gofra2003 <121211543+Gofra2003@users.noreply.github.com>
2025-07-16 19:21:53 -0500

    Cambio en la Zona educativa
    Había un nombre mal puesto en una propiedad del iframe del segundo video y lo cambie xd

--- fe-ecorecicla/src/app/ZonaEducativa/page.jsx
+++ fe-ecorecicla/src/app/ZonaEducativa/page.jsx
@@ -47,7 +47,7 @@ export default function ZonaEducativa() {
                 title="YouTube video player" 
                 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                 referrerpolicy="strict-origin-when-cross-origin"
+                 referrerPolicy="strict-origin-when-cross-origin"
                 allowfullscreen></iframe>
               </div>
             </div>
